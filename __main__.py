430dff73f135b32d89ae0c353e58457809d7fa1a
Nataly <sunflower-n@mail.ru>
2024-03-18 15:46:27 +0100

    2

--- __main__.py
+++ __main__.py
@@ -2,23 +2,20 @@ import logging
 import mysql.connector
 import json
 
-#while True:
-#    i=input('[1] Export to json\n[2] Export to XML\nSelect export file type: ')
-#    if i == '1':
-#        a=[{'id':1, 'name':'1'},{'id':2, 'name':'2'}]
-#        with open('output.json', 'w') as file:
-#            json.dump(a, file)
-#        break
-
 create_db_query = 'CREATE DATABASE hostel'
 create_rooms_table_query = 'CREATE TABLE rooms (id INT PRIMARY KEY, name VARCHAR(15))'
 create_students_table_query = 'CREATE TABLE students (birthday DATETIME, id INT PRIMARY KEY, name VARCHAR(50), room INT, sex CHAR(1), FOREIGN KEY(room) REFERENCES rooms(id))'
+select_query_1 = 'SELECT rooms.name, COUNT(students.id) AS count_students FROM rooms LEFT JOIN students ON rooms.id = students.room GROUP BY rooms.id ORDER BY rooms.name;'
+select_query_2 = 'SELECT rooms.name, ROUND(AVG((DATEDIFF(NOW(), birthday)) / 365), 2) AS average_age FROM rooms LEFT JOIN students ON rooms.id = students.room GROUP BY rooms.id ORDER BY average_age, rooms.id LIMIT 5;'
+select_query_3 = 'SELECT rooms.name, ROUND(MAX((DATEDIFF(NOW(), birthday)) / 365) - MIN((DATEDIFF(NOW(), birthday)) / 365), 2) AS difference_age FROM rooms LEFT JOIN students ON rooms.id = students.room GROUP BY rooms.id ORDER BY difference_age DESC, rooms.id LIMIT 5;'
+select_query_4 = 'SELECT rooms.name FROM rooms LEFT JOIN students ON rooms.id = students.room WHERE students.sex IN ("M") AND students.sex IN ('F') GROUP BY rooms.id;'
 
 try:
-    mydb = mysql.connector.connect(user='root', password='tptoor-0078', host='127.0.0.1')
+    mydb = mysql.connector.connect(user='root', password='sql2150', host='127.0.0.1', auth_plugin = 'mysql_native_password')
     logging.debug('Успешное подключение к БД')
-except:
-    logging.critical('Не удалось подключиться к дб')
+except Exception as ex:
+    print(ex)
+    logging.critical('Не удалось подключиться к БД')
 
 try:
     with mydb.cursor() as cur:
@@ -32,17 +29,15 @@ try:
         cur.execute(create_rooms_table_query)
         cur.execute(create_students_table_query)
         mydb.commit()
-except:pass
+except: pass
 
-
-
-#rooms_file = open(input('Enter rooms file path: '), 'r')
-#data_rooms_file = json.load(rooms_file)
-#for i in data_rooms_file:
-#    insert_rooms_query = 'INSERT INTO rooms VALUES (%s, %s)'
-#    values = (i['id'], i['name'],)
-#    cur.execute(insert_rooms_query, values)
-#mydb.commit()
+rooms_file = open(input('Enter rooms file path: '), 'r')
+data_rooms_file = json.load(rooms_file)
+for i in data_rooms_file:
+    insert_rooms_query = 'INSERT INTO rooms VALUES (%s, %s)'
+    values = (i['id'], i['name'],)
+    cur.execute(insert_rooms_query, values)
+mydb.commit()
 
 students_file = open(input('Enter students file path: '), 'r')
 data_students_file = json.load(students_file)
@@ -55,3 +50,12 @@ with mydb.cursor() as cur:
 
 cur.close()
 mydb.close()
+
+
+#while True:
+#    i=input('[1] Export to json\n[2] Export to XML\nSelect export file type: ')
+#    if i == '1':
+#        a=[{'id':1, 'name':'1'},{'id':2, 'name':'2'}]
+#        with open('output.json', 'w') as file:
+#            json.dump(a, file)
+#        break
